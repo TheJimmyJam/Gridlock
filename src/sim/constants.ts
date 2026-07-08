88d018fab4ab1b7a4aaf887c614b5182da8b8f2c
Jimmy Cannon <wcannon83@gmail.com>
2026-07-08 13:37:18 -0500

    Bump grid to 200x200 for more build room

--- src/sim/constants.ts
+++ src/sim/constants.ts
@@ -1,5 +1,5 @@
-export const GRID_COLS = 40;
-export const GRID_ROWS = 40;
+export const GRID_COLS = 200;
+export const GRID_ROWS = 200;
 
 /** Sim runs on a fixed timestep, decoupled from render framerate. */
 export const TICK_INTERVAL_MS = 250;
